6156fcfdaf49e3b54284501eca4af8fc62855ade
gabriel marques <gabrielmarksf@icloud.com>
2025-09-10 00:11:34 -0300

    Implementing the NavGraph in main activity

--- app/src/main/java/br/com/littlepig/MainActivity.kt
+++ app/src/main/java/br/com/littlepig/MainActivity.kt
@@ -1,14 +1,27 @@
 package br.com.littlepig
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import br.com.littlepig.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container)
+                as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
