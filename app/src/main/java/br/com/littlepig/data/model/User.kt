d961c429af5d096209b1c528056066b86f11adab
gabriel marques <gabrielmarksf@icloud.com>
2025-09-29 21:08:05 -0300

    Create User data

---
+++ app/src/main/java/br/com/littlepig/data/model/User.kt
@@ -0,0 +1,12 @@
+package br.com.littlepig.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class User(
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("password")
+    val password: String,
+    @SerializedName("email")
+    val email: String
+)
