d9ef7f29ad8ad9de08efb68e6a30fc146658e20d
gabriel marques <gabrielmarksf@icloud.com>
2025-10-02 23:35:02 -0300

    Adding DataStore dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
     implementation(libs.converter.gson)
     implementation(libs.okhttp)
     implementation(libs.logging.interceptor)
+    implementation(libs.androidx.datastore.preferences)
     kapt(libs.hilt.compiler)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
