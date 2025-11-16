a7b19a6d5b8c9d9785855c68faf4f77799cfd206
gabriel marques <gabrielmarksf@icloud.com>
2025-11-16 13:34:38 -0300

    Refactoring user repository to handle network errors

--- app/src/main/java/br/com/littlepig/data/repository/user/IUserRepository.kt
+++ app/src/main/java/br/com/littlepig/data/repository/user/IUserRepository.kt
@@ -1,19 +1,20 @@
 package br.com.littlepig.data.repository.user
 
+import br.com.littlepig.common.Result
+import br.com.littlepig.data.enums.NetworkError
 import br.com.littlepig.data.model.User
 import br.com.littlepig.data.model.UserLoginRequest
 import br.com.littlepig.data.model.UserLoginResponse
 import br.com.littlepig.data.model.UserRegisterRequest
-import br.com.littlepig.data.model.balance.Balance
-import br.com.littlepig.data.model.balance.DeleteResponse
-import br.com.littlepig.data.model.balance.TransactionRequest
 import br.com.littlepig.data.model.balance.UserBalanceResponseItem
-import retrofit2.Response
 
 interface IUserRepository {
-    suspend fun registerUser(user: UserRegisterRequest): User
+    suspend fun registerUser(user: UserRegisterRequest): Result<User, NetworkError>
 
-    suspend fun login(user: UserLoginRequest): UserLoginResponse
+    suspend fun login(user: UserLoginRequest): Result<UserLoginResponse, NetworkError>
 
-    suspend fun getUserBalance(date: String, token: String): Response<List<UserBalanceResponseItem>>
+    suspend fun getUserBalance(
+        date: String,
+        token: String
+    ): Result<List<UserBalanceResponseItem>, NetworkError>
 }
--- app/src/main/java/br/com/littlepig/data/repository/user/UserRepository.kt
+++ app/src/main/java/br/com/littlepig/data/repository/user/UserRepository.kt
@@ -1,27 +1,56 @@
 package br.com.littlepig.data.repository.user
 
+import br.com.littlepig.common.Result
+import br.com.littlepig.common.asResultError
 import br.com.littlepig.data.UserService
+import br.com.littlepig.data.enums.NetworkError
 import br.com.littlepig.data.model.User
 import br.com.littlepig.data.model.UserLoginRequest
 import br.com.littlepig.data.model.UserLoginResponse
 import br.com.littlepig.data.model.UserRegisterRequest
-import br.com.littlepig.data.model.balance.Balance
-import br.com.littlepig.data.model.balance.DeleteResponse
-import br.com.littlepig.data.model.balance.TransactionRequest
 import br.com.littlepig.data.model.balance.UserBalanceResponseItem
-import retrofit2.Response
+import retrofit2.HttpException
 import javax.inject.Inject
 
 class UserRepository @Inject constructor(
     private val service: UserService
 ) : IUserRepository {
-    override suspend fun registerUser(user: UserRegisterRequest): User = service.register(user)
+    override suspend fun registerUser(user: UserRegisterRequest): Result<User, NetworkError> {
+        return try {
+            val response = service.register(user)
 
-    override suspend fun login(user: UserLoginRequest): UserLoginResponse = service.login(user)
+            response.body()?.let { userResponse ->
+                Result.Success(userResponse)
+            } ?: Result.Error(NetworkError.EMPTY_RESPONSE)
+        } catch (error: HttpException) {
+            error.asResultError()
+        }
+    }
+
+    override suspend fun login(user: UserLoginRequest): Result<UserLoginResponse, NetworkError> {
+        return try {
+            val response = service.login(user)
+
+            response.body()?.let { userLoginResponse ->
+                Result.Success(userLoginResponse)
+            } ?: Result.Error(NetworkError.EMPTY_RESPONSE)
+        } catch (error: HttpException) {
+            error.asResultError()
+        }
+    }
 
     override suspend fun getUserBalance(
         date: String,
         token: String
-    ): Response<List<UserBalanceResponseItem>> =
-        service.getUserBalance(date, token)
+    ): Result<List<UserBalanceResponseItem>, NetworkError> {
+        return try {
+            val response = service.getUserBalance(date, token)
+
+            response.body()?.let { userBalance ->
+                Result.Success(userBalance)
+            } ?: return Result.Error(NetworkError.EMPTY_RESPONSE)
+        } catch (error: HttpException) {
+            error.asResultError()
+        }
+    }
 }
